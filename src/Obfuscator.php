1c5f91a736506f8ce885e0f3f04fe9c8c092be7a
Fabian Mundt <f.mundt@inventionate.de>
2019-08-15 18:35:33 +0200

    Add CSP support.
    Insert csp_nonce at script tag.

--- src/Obfuscator.php
+++ src/Obfuscator.php
@@ -37,7 +37,7 @@ function obfuscateEmail($string)
             $parts = array_map('trim', $parts);
 
             // ROT13 implementation for JS-enabled browsers
-            $js = '<script>Rot13.write(' . "'" . str_rot13($parts[0]) . "'" . ');</script>';
+            $js = '<script nonce="' . csp_nonce() . '">Rot13.write(' . "'" . str_rot13($parts[0]) . "'" . ');</script>';
 
             // Reversed direction implementation for non-JS browsers
             if (stripos($parts[0], '<a') === 0) {
